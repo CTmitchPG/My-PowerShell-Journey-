729a32fbf64293cd21350ba2349118c30d05c015
CTmitchPG <134589307+CTmitchPG@users.noreply.github.com>
2025-02-20 17:07:53 -0500

    test2

---
+++ exchangeadmin/emailnotification.ps1
@@ -0,0 +1,6 @@
+#Install Exchange Online PowerShell Module:
+#Import the Module
+Import-Module ExchangeOnlineManagement
+#Connect to Exchange Online
+Connect-ExchangeOnline -UserPrincipalName youradmin@yourdomain.com
+Ensure your account has the necessary roles (Exchange Admin, Groups Admin, or Global Admin)
